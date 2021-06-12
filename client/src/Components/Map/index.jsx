4d4190a2e0f98dde06e2d0abfe4294f79762f60e
theqoobee <qoobeethegreat@gmail.com>
2021-06-12 21:59:08 +0200

    attempt to make the map work :'(

--- client/src/Components/Map/index.jsx
+++ client/src/Components/Map/index.jsx
@@ -1,37 +1,36 @@
-import ReactMapboxGl from 'react-mapbox-gl';
-import './style.scss';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css"
+import ReactMapboxGl from "react-mapbox-gl"
+import "./style.scss"
 
 const Map = ReactMapboxGl({
-  accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
-});
+	accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+})
 
-const Maps = (props) => {
-  return (
-    <Map
-      antialias={true}
-      containerStyle={{
-        height: '70vh',
-        width: '66%',
-        position: 'absolute',
-        right: '4.5vw',
-        top: '17vh',
-        transition: '.5s',
-        overflow: 'hidden',
-        borderRadius: '15px',
-        bearingL: 0
-      }}
-      center={[-117.06651266267941, 32.76570649214452]}
-      flyToOptions={{
-        speed: 2
-        }}
-      onClick={props.mapClick}
-      onStyleLoad={props.mapLoad}
-      pitch = {[60]}
-      style="mapbox://styles/mapbox/light-v10"
-      zoom = {[16]}
-    >
-    </Map>
-    );
-  }
-export default Maps;
+const Maps = props => {
+	return (
+		<Map
+			antialias={true}
+			containerStyle={{
+				height: "70vh",
+				width: "66%",
+				position: "absolute",
+				right: "4.5vw",
+				top: "17vh",
+				transition: ".5s",
+				overflow: "hidden",
+				borderRadius: "15px",
+				bearingL: 0,
+			}}
+			center={[32, 32.1]}
+			flyToOptions={{
+				speed: 2,
+			}}
+			onClick={props.mapClick}
+			onStyleLoad={props.mapLoad}
+			pitch={[60]}
+			// eslint-disable-next-line react/style-prop-object
+			style="mapbox://styles/mapbox/light-v10"
+			zoom={[16]}></Map>
+	)
+}
+export default Maps
